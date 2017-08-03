0d8f4ff64506e709b39c13643a35e31d6ba2e20e
Guihu <850589779@qq.com>
2017-08-03 10:02:29 +0800

    缺少jstl jar包

--- src/com/gui/controller/TestController.java
+++ src/com/gui/controller/TestController.java
@@ -1,18 +1,20 @@
-package com.xrq.controller;
+package com.gui.controller;
 
+import com.gui.entity.Test;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/test")
 public class TestController
 {
-    @RequestMapping
-    public String dispatchTest()
-    {
-        System.out.println("Enter TestController.dispatchTest");
-        return "test";
-    }
+    //@RequestMapping
+    //public String dispatchTest()
+    //{
+    //    System.out.println("Enter TestController.dispatchTest");
+    //    return "test";
+    //}
 
     @RequestMapping(value = "/add")
     public String dispatchAddTest()
@@ -41,4 +43,20 @@ public class TestController
         System.out.println("Enter TestController.dispatchTest(), test: " + test);
         return "redirect:/test2.jsp?test="+test;
     }
+
+    @RequestMapping
+    public String dispatchTest(Test test, Model model)
+    {
+        model.addAttribute("modelKey", "modelValue");
+        return "test3";
+    }
+
+    //重定向
+    //@RequestMapping
+    //public String dispatchTest(Test test, HttpSession session)
+    //{
+    //    session.setAttribute("modelKey", "modelValue");
+    //    return "redirect:/test.jsp";
+    //    //return "test";
+    //}
 }
